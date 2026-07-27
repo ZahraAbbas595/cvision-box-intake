341c40b5f6feddd0c3839f7097cf4efc1d1d74c9
ZahraAbbas595 <zahrafreelance9595@gmail.com>
2026-07-27 12:47:23 +0500

    test(api): add local inference test script

---
+++ tests/test_infer.py
@@ -0,0 +1,11 @@
+﻿import requests
+
+files = {'file': open('eval/dataset/images/img_001.jpg', 'rb')}
+r = requests.post('http://localhost:8000/v1/box-intake/infer', files=files)
+data = r.json()
+print('status:', r.status_code)
+print('visible_box_count:', data['visible_box_count'])
+print('confidence_score:', data['confidence_score'])
+print('human_review_required:', data['human_review_required'])
+print('review_reasons:', data['review_reasons'])
+print('processing_time_ms:', data['processing_time_ms'])
